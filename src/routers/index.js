09f6487d5430af60f05b1f31581d6f8632cd3e96
ngotruonganh <ngotruonganh1901@gmail.com>
2022-12-18 17:22:23 +0700

    Clear code

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,19 +1,9 @@
 import Home from "../pages/Home";
-import ParentTest from "../pages/ParentTest";
-import Error from "../pages/Error";
 
 const publicRoutes = [
   {
     path: "/",
     component: Home,
-  },
-  {
-    path: "/parenttest",
-    component: ParentTest,
-  },
-  {
-    path: "*",
-    component: Error,
   },
 ];
 
