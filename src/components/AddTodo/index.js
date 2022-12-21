740d3d973a0d3279ef4e052cc6ed44dd356b823c
ngotruonganh <ngotruonganh1901@gmail.com>
2022-12-22 03:15:06 +0700

    Update des

--- src/components/AddTodo/index.js
+++ src/components/AddTodo/index.js
@@ -5,66 +5,110 @@ import "./AddTodo.scss";
 class AddTodo extends React.Component {
   state = {
     title: "",
+    des: "",
   };
 
   // thay doi state title moi khi nhap vao input
-  handleOnChange = (event) => {
+  handleOnChangeTitle = (event) => {
     this.setState({
       title: event.target.value,
     });
   };
 
-  // submit button
+  // thay doi state des moi khi nhap vao input
+  handleOnChangeDes = (event) => {
+    this.setState({
+      des: event.target.value,
+    });
+  };
+
+  // Su kien tao moi cong viec (click chuot)
   handleClick = () => {
+    // truong hop khong nhap gi
+    if (!this.state.title && !this.state.des) {
+      toast.error("Vui lòng điền đầy đủ tiêu để và nội dung");
+      return;
+    }
+    // truong hop chi nhap vao title
     if (!this.state.title) {
-      toast.error("Chưa nhập công việc");
+      toast.error("Chưa nhập tiêu đề");
+      return;
+    }
+    // truong hop chi nhap vao des
+    if (!this.state.des) {
+      toast.error("Chưa nhập nội dung");
       return;
     }
     let todo = {
       id: Math.floor(Math.random() * 1001),
       title: this.state.title,
+      des: this.state.des,
     };
-
     // truyen ham addnewtodo
     this.props.addNewToDo(todo);
     //set state lai
     this.setState({
       title: "",
+      des: "",
     });
   };
 
-  // enter press
+  //   // Su kien tao moi cong viec (nhan enter)
   enterkey = (event) => {
     if (event.key === "Enter") {
+      // truong hop khong nhap gi
+      if (!this.state.title && !this.state.des) {
+        toast.error("Vui lòng điền đầy đủ tiêu để và nội dung");
+        return;
+      }
+      // truong hop chi nhap vao title
       if (!this.state.title) {
-        toast.error("Chưa nhập công việc");
+        toast.error("Chưa nhập tiêu đề");
+        return;
+      }
+      // truong hop chi nhap vao des
+      if (!this.state.des) {
+        toast.error("Chưa nhập nội dung");
         return;
       }
       let todo = {
         id: Math.floor(Math.random() * 1001),
         title: this.state.title,
+        des: this.state.des,
       };
       this.props.addNewToDo(todo);
       this.setState({
         title: "",
+        des: "",
       });
     }
   };
 
   render() {
-    let { title } = this.state;
+    let { title, des } = this.state;
     return (
       <>
         <div className="input-field">
-          <input
-            type="text"
-            value={title}
-            onChange={(event) => this.handleOnChange(event)}
-            onKeyPress={(event) => this.enterkey(event)}
-            placeholder="Thêm công việc mới"
-          />
+          <div className="type-field">
+            <input
+              type="text"
+              value={title}
+              onChange={(event) => this.handleOnChangeTitle(event)}
+              onKeyDown={(event) => this.enterkey(event)}
+              placeholder="Nhập tiêu đề công việc mới"
+            />
+            <input
+              type="text"
+              value={des}
+              onChange={(event) => this.handleOnChangeDes(event)}
+              onKeyDown={(event) => this.enterkey(event)}
+              placeholder="Nhập nội dung công việc mới"
+              className="des-type"
+            />
+          </div>
           <button type="button" onClick={() => this.handleClick()}>
             <i className="fa-solid fa-plus"></i>
+            {"  "}Add
           </button>
         </div>
       </>
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -6,24 +6,69 @@ import AddTodo from "../../components/AddTodo";
 class Home extends React.Component {
   state = {
     listToDo: [
-      { id: "1", title: "Wake up" },
-      { id: "2", title: "Eating" },
-      { id: "3", title: "Working" },
-      { id: "4", title: "Sleeping" },
-      { id: "5", title: "Watching" },
-      { id: "6", title: "Playing" },
-      { id: "7", title: "Swimming" },
-      { id: "8", title: "Holding" },
-      { id: "9", title: "Do homework" },
+      { id: "1", title: "Wake up", des: "wake up in 5am" },
+      {
+        id: "2",
+        title: "Eating",
+        des: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled",
+      },
+      {
+        id: "3",
+        title: "Working",
+        des: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
+      },
+      {
+        id: "4",
+        title: "Sleeping",
+        des: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
+      },
+      { id: "5", title: "Watching", des: "Waching football" },
+      {
+        id: "6",
+        title: "Playing",
+        des: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
+      },
+      {
+        id: "7",
+        title: "Swimming",
+        des: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
+      },
+      { id: "8", title: "Do homework", des: "Coding" },
     ],
+    title: "",
+    des: "",
     editToDo: {},
     onModal: false,
   };
+  onChangTitle = (e) => {
+    this.setState({
+      title: e.target.value,
+    });
+  };
+  onChangDes = (e) => {
+    this.setState({
+      des: e.target.value,
+    });
+  };
 
   // Add new to do function
-  addNewToDo = (todo) => {
+  handleClick = () => {
+    let object = {
+      id: Math.floor(Math.random() * 1001),
+      title: this.state.title,
+      des: this.state.des,
+    };
+    let ToDoList = localStorage.getItem("todo");
+    if (ToDoList) {
+      let arr = JSON.parse(ToDoList);
+      arr.push(object);
+      localStorage.setItem("todo", JSON.stringify(arr));
+    } else {
+      localStorage.setItem("todo", JSON.stringify([object]));
+    }
     this.setState({
-      listToDo: [...this.state.listToDo, todo],
+      title: "",
+      des: "",
     });
     toast.success("Thêm thành công");
   };
@@ -80,16 +125,42 @@ class Home extends React.Component {
     });
   };
 
+  addNewToDo = (todo) => {
+    this.setState({
+      listToDo: [...this.state.listToDo, todo],
+    });
+    toast.success("Thêm thành công");
+  };
+
   render() {
     let { listToDo, editToDo, onModal } = this.state;
     let isEmptyObj = Object.keys(editToDo).length === 0;
+    let toDoList = localStorage.getItem("todo");
+    console.log("state", listToDo);
+    console.log("local", toDoList);
     return (
       <>
         <div className="input-container">
           <div>
             <h1> To Do List </h1>
           </div>
+          {/* <input
+            value={this.state.title}
+            placeholder="add new todo"
+            onChange={(e) => this.onChangTitle(e)}
+          />
+          <input
+            value={this.state.des}
+            placeholder="add des"
+            onChange={(e) => this.onChangDes(e)}
+          />
+          <button type="button" onClick={() => this.handleClick()}>
+            <i className="fa-solid fa-plus"></i>
+          </button>
+          {toDoList} */}
+          {/* <hr /> */}
           <AddTodo addNewToDo={this.addNewToDo} />
+          <br />
           <div className="list-todo">
             {listToDo &&
               listToDo.length > 0 &&
@@ -97,9 +168,14 @@ class Home extends React.Component {
                 return (
                   <div className="todo-list-container" key={item.id}>
                     {isEmptyObj === true ? (
-                      <span>
-                        {index + 1} - {item.title}
-                      </span>
+                      <>
+                        <div className="info">
+                          <p className="title">
+                            {index + 1} - {item.title}
+                          </p>
+                          <p className="des">{item.des}</p>
+                        </div>
+                      </>
                     ) : (
                       <>
                         {editToDo.id === item.id ? (
@@ -130,21 +206,18 @@ class Home extends React.Component {
                           <i className="fa-solid fa-pen-to-square"></i>
                         )}
                       </button>
-                      <button onClick={() => this.onModal(item)}>
+                      <button onClick={() => this.onModal()}>
                         <i className="fa-solid fa-circle-info"></i>
                       </button>
                       {onModal === true && (
-                        <div
-                          className="model"
-                          //   onClick={() => this.onModal(item)}
-                        >
+                        <div className="model">
                           <div className="overlay"></div>
                           <div className="popup">
                             <i
-                              className="fa-solid fa-circle-xmark"
-                              onClick={() => this.onModal(item)}
+                              className="close-btn fa-solid fa-circle-xmark"
+                              onClick={() => this.onModal()}
                             ></i>
-                            {item.title}
+                            <h1>{item.title}</h1>
                           </div>
                         </div>
                       )}
